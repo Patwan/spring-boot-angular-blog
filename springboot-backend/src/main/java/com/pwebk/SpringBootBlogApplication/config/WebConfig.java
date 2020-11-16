257b777a49f792440f8e63c95fe95b87441b30ba
Martin Njogu <mndungu730@gmail.com>
2020-11-16 07:54:27 +0300

    Added Spring Backend CORS Middleware

---
+++ springboot-backend/src/main/java/com/pwebk/SpringBootBlogApplication/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.pwebk.SpringBootBlogApplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    //Cors Request setting, here we accept requests from all incoming request to our backend.
+    //If developing a production grade application, should restrict to fewer requests
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(3600L)
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
