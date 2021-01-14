7a4fd536cb458cde4e5975beba30b82856bae4f5
Martin Njogu <mndungu730@gmail.com>
2021-01-14 10:18:31 +0300

    Refactored Code in AuthService

--- angular-frontend/src/app/auth/login/login-response.payload.ts
+++ angular-frontend/src/app/auth/login/login-response.payload.ts
@@ -1,3 +1,5 @@
+//An Interface is a specification that identifies a related set of
+//properties and methods to be implemented by a class. Concept in OOP but used in typescript
 export interface LoginResponse {
     authenticationToken: string;
     refreshToken: string;
--- angular-frontend/src/app/auth/signup/singup-request.payload.ts
+++ angular-frontend/src/app/auth/signup/singup-request.payload.ts
@@ -1,4 +1,5 @@
-//SignUpRequestPayload model that holds data and distributes data within the application
+//An Interface is a specification that identifies a related set of
+//properties and methods to be implemented by a class. Concept in OOP but used in typescript
 export interface SignupRequestPayload {
     username: string;
     password: string;
